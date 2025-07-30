2edc98e9ac33683032e1902598544482bd266bc6
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-07-30 10:48:30 +0100

    create base command
    Co-authored-by: Salahaddin Ebrahimipour <salahebrahimipour@users.noreply.github.com>
    Co-authored-by: Junaid Abdul Jabbar <junaidjabbar01@users.noreply.github.com>

---
+++ arches/management/commands/bulk_approve.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    """
+    Commands for managing the loading and running of packages in Arches
+
+    """
+
+    def add_arguments(self, parser):
+        pass
