c47dccd29fe941d0afb39947aacf9843ea185a65
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-08-13 09:29:20 +0100

    add option for user_names

--- arches/management/commands/bulk_approve.py
+++ arches/management/commands/bulk_approve.py
@@ -22,14 +22,37 @@ class Command(BaseCommand):
             nargs="+",
             help="One or more user IDs to approve edits for (separate by space)",
         )
+        parser.add_argument(
+            "-n",
+            "--user_names",
+            type=str,
+            nargs="+",
+            help="One or more user names to approve edits for (separate by space)",
+        )
 
     def handle(self, *args, **options):
         user_ids = options.get("user_ids")
+        user_names = options.get("user_names")
 
-        if not user_ids:
-            raise CommandError("You must provide at least one user_id argument.")
+        if user_ids and user_names:
+            raise CommandError(
+                "You must provide either user_ids OR user_names argument, not both."
+            )
 
+        if not user_ids and not user_names:
+            raise CommandError(
+                "You must provide at least one user_id or user_name argument."
+            )
         User = get_user_model()
+        if user_names:
+            user_ids = User.objects.filter(username__in=user_names).values_list(
+                "id", flat=True
+            )
+            if not user_ids:
+                raise CommandError(
+                    f"User(s) with name(s) {user_names} do(es) not exist."
+                )
+
         for user_id in user_ids:
             if not User.objects.filter(pk=user_id).exists():
                 self.stdout.write(
@@ -39,7 +62,7 @@ class Command(BaseCommand):
             if not user_has_provisional_edits(user_id):
                 self.stdout.write(
                     self.style.NOTICE(
-                        f"No provisional edits found for user ID {user_id}."
+                        f"No provisional edits found for user ID {user_id} or username {User.objects.filter(pk=user_id).first().username}"
                     )
                 )
                 continue
@@ -47,6 +70,6 @@ class Command(BaseCommand):
             approve_all_provisional_edits_for_user(user_id)
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"All provisional edits for user ID {user_id} have been approved."
+                    f"All provisional edits for user ID {user_id} or username {User.objects.filter(pk=user_id).first().username} have been approved."
                 )
             )
